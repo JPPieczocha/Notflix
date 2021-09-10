0a3abfa5e3ffaf0394b3dba44e248efb3ed781cc
Matias <mati_d_pinto@hotmail.com>
2021-09-10 19:44:08 -0300

    MoviePlayer mods

--- src/pages/moviePlayer/MoviePlayer.js
+++ src/pages/moviePlayer/MoviePlayer.js
@@ -1,8 +1,6 @@
 import React,{useState, useRef } from 'react';
-import { View, StyleSheet, Button, Dimensions, TouchableWithoutFeedback, Text, TouchableOpacity,Animated, Platform } from 'react-native';
+import { View, StyleSheet, Dimensions, Platform } from 'react-native';
 import { Video, AVPlaybackStatus, ScreenOrientation } from 'expo-av';
-import { Ionicons } from '@expo/vector-icons';
-import Colors from '../../constants/colors';
 
 const {width, height} = Dimensions.get('window')
 
@@ -10,36 +8,15 @@ const MoviePlayer = ({navigation, route})=>{
 
 	const {title} = route.params;
 	const [fullScreen,setFullScreen] = useState(false);
-	const [orientationIsLandscape, setOrientationIsLandscape] = useState(false);
-	const [topLayer,setTopLayer] = useState(true);
-	const video = React.useRef(null);
 	const [status, setStatus] = React.useState({});
-	const fadeAnim = useRef(new Animated.Value(1)).current;
 
-	const handleTouchDisplay = ()=>{
-		Animated.timing(fadeAnim, {
-			toValue: topLayer ? 0 : 1,
-			duration: 700,
-			useNativeDriver: true
-		}).start(({ finished })=>{
-			setTopLayer(!topLayer)
-		});
-	}
+	const video = React.useRef(null);
 
 	const fullScreenPlayback = ()=>{
 		if(Platform.OS === 'ios'){
 			video.current.presentFullscreenPlayer()
-		}
-		// }else{
-		// 	await ScreenOrientation.lockAsync(orientationIsLandscape ? ScreenOrientation.OrientationLock.PORTRAIT : ScreenOrientation.OrientationLock.LANDSCAPE_LEFT,);
-		// 	setOrientationIsLandscape(!orientationIsLandscape);
-		// }
-	}
+		}else{
 
-	const handleAndroid = (params)=>{
-		if(Platform.OS === 'android'){
-			params.naturalSize.orientation = "landscape";
-			console.log("params---->", params.naturalSize.orientation);
 		}
 	}
 
@@ -52,6 +29,8 @@ const MoviePlayer = ({navigation, route})=>{
 			if(status.isLoaded && fullScreen){
 				navigation.pop();
 			}
+		}else{
+
 		}
 	}
 
@@ -62,64 +41,13 @@ const MoviePlayer = ({navigation, route})=>{
 		return true;
 	}
 
-	const videoLayer = ()=>{
-		if(topLayer){
-			return(
-				<Animated.View style={{
-					position:'absolute',
-					top:0,
-					left:-220, //FEO
-					height:width, 
-					width:height, 
-					backgroundColor:Colors.transparentBlack,
-					zIndex:1000,
-					justifyContent:'space-between',
-					opacity: fadeAnim
-					}}
-					>
-					<View style={styles.headerLayer}>
-						<TouchableOpacity style={styles.backButton} onPress={()=>handleBackButton()}>
-							<Ionicons name={'arrow-back'} size={30} color={Colors.inactiveTint}/>
-						</TouchableOpacity>
-						<Text style={styles.headerLayerText}>{title}</Text>
-						<Text>              </Text> 
-						{/* Lo de arriba es prueba */}
-					</View>
-
-					<View style={styles.footerLayer}>
-						<View style={styles.buttonsFooter}>
-							<TouchableOpacity style={styles.backButton} onPress={()=>handleBackwardVideo()}>
-								<Ionicons name="ios-play-back-sharp" size={30} color={Colors.inactiveTint} />
-
-							</TouchableOpacity>
-							
-							<TouchableOpacity style={styles.backButton} onPress={()=>handlePlay()}>
-								<Ionicons name={status.isPlaying ? 'pause' : 'play'} size={30} color={Colors.inactiveTint}/>
-							</TouchableOpacity>
-
-							{/* <TouchableOpacity style={styles.backButton} onPress={()=>prueba()}> */}
-							<TouchableOpacity style={styles.backButton} onPress={()=>handleForwardVideo()}>
-								<Ionicons name="ios-play-forward-sharp" size={30} color={Colors.inactiveTint} />
-							</TouchableOpacity>
-						</View>
-						<Text style={styles.headerLayerText}>--{hours} : {minutes} : {seconds}----</Text>
-						{/* Lo de arriba es prueba */}
-					</View>
-				</Animated.View>
-			)
-		}
-	}
-
 	return (
 		<View style={styles.container}>
-		<TouchableWithoutFeedback onPress={()=>handleTouchDisplay()}>
 			<View onPress={()=>console.log('TOQUE')}>
 				<Video
 					ref={video}
 					style={styles.video}
-					source={{
-					uri: 'https://es.vid.web.acsta.net/nmedia/34/19/06/03/14//19562331_hd_013.mp4'
-					,}}
+					source={{uri: 'https://es.vid.web.acsta.net/nmedia/34/19/06/03/14//19562331_hd_013.mp4',}}
 					resizeMode="contain"
 
 					//Y si lo pongo en pantalla chiquita y doy la chance de que hagan fullscreen?? MMm
@@ -129,11 +57,10 @@ const MoviePlayer = ({navigation, route})=>{
 					useNativeControls={handlePlatformNativeControls()}
 					onLoad={()=>fullScreenPlayback()}
 					onFullscreenUpdate={()=>handleDismissFullScreen()}
-					// onReadyForDisplay={params => handleAndroid(params)}
-					onReadyForDisplay={params => {params.naturalSize.orientation = "landscape"}}
+					// onReadyForDisplay={params => {params.naturalSize.orientation = "landscape"}}
+					// el onReadyForDisplay era  una solución que no funcionó de una web.
 				/>
 			</View>
-		</TouchableWithoutFeedback>
 		</View>
 	);
 }
@@ -143,53 +70,13 @@ const styles = StyleSheet.create({
 		flex: 1,
 		justifyContent: 'center',
 		backgroundColor: 'rgba(0,0,0,0)',
-		// transform: [{ rotate: '90deg' }]
 	},
 	video: {
-	alignSelf: 'center',
-	// width: height,
-	// height: width,
-	width: width,
-	height: height,
-	backgroundColor:'#000000'
-	},
-	buttons: {
-		flexDirection: 'row',
-		justifyContent: 'center',
-		alignItems: 'center',
-	},
-	headerLayer:{
-		height:80,
-		flexDirection:'row',
-		alignItems:'center',
-		justifyContent:'space-around'
-	},
-	headerLayerText:{
-		color:Colors.white,
-		fontSize: 30
-	},
-	backButton:{
-        width:50,
-        height:50,
-        backgroundColor: Colors.transparentWhite,
-        justifyContent:'center',
-        alignItems:'center',
-    },
-	footerLayer:{
-		marginBottom: 10,
-
-		flexDirection:'row',
-		alignItems:'center',
-		justifyContent:'center'
-	},
-	buttonsFooter:{
-		width: '25%',
-
-		flexDirection: 'row',
-		alignItems: 'center',
-		justifyContent:'space-around'
+		alignSelf: 'center',
+		width: width,
+		height: height,
+		backgroundColor:'#000000'
 	}
-
 });
 
 export default MoviePlayer;
