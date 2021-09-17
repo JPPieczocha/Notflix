99621f92124b73c4914809d6fc32e2f1d4d3943d
Matias <mati_d_pinto@hotmail.com>
2021-09-17 18:58:25 -0300

    useContext added

--- App.js
+++ App.js
@@ -1,5 +1,5 @@
 import { StatusBar } from 'expo-status-bar';
-import React, {useState, createContext, useMemo} from 'react';
+import React, {useState, createContext, useMemo, useEffect} from 'react';
 import { StyleSheet, Text, View } from 'react-native';
 import { NavigationContainer } from '@react-navigation/native';
 import { createNativeStackNavigator } from '@react-navigation/native-stack';
@@ -7,28 +7,55 @@ import Colors from './src/constants/colors'
 import NavLogSign from './src/navigation/NavLogSign';
 import MainNav from './src/navigation/MainNav';
 import LoadingPage from './src/components/loadingPage/LoadingPage';
-
-
-const AuthContext = createContext();
-
-
+import * as SecureStore from 'expo-secure-store';
+import { UserContext } from './src/components/context/authContext';
 
 export default function App() {
 
+	const [userData, setUserData] = useState({});
 	const [isLoading, setisLoading] = useState(true)
 	const [logueado, setLogueado] = useState(false)
 	//lo de arriba (logueado) se va, es un dummy, demostrará si me en cuentro logueado o no a la hora de qué pantallas mostrar
+
+	useEffect(() => {
+
+		const getTokenAsync = async () =>{
+			//Solo si la sesión ya se encuentra iniciada.
+			let userToken;
+			try{
+				userToken = await SecureStore.getItemAsync('userToken');
+			}catch (e){
+ 				// Restoring token failed
+				 console.log('Dentro de carch: '+e);
+			}
+			if(userToken === null){ //cambiar a ver si está iniciada la sesión
+				setUserData({nombre: 'Nicolas', apellido: 'ASDASDASD', mail:'nm@gmail.com'})
+				setLogueado(!logueado)
+				setisLoading(!isLoading)
+			}else{
+				setUserData({nombre: 'Nicolas', apellido: 'ASDASDASD', mail:'nm@gmail.com'})
+				setisLoading(!isLoading)
+			}
+
+		}
+		// After restoring token, we may need to validate it in production apps
+
+     	// This will switch to the App screen or Auth screen and this loading
+      	// screen will be unmounted and thrown away.
+		  getTokenAsync()
+	}, [])
+
 
 	const Stack = createNativeStackNavigator();
 	
 	return (
 		<NavigationContainer>
+			<UserContext.Provider value={userData}>
 			<Stack.Navigator>
-				{!isLoading ? <Stack.Screen name="Loading" component={LoadingPage} options={{headerShown:false}}/> : null}
-					{logueado ? 
+				{isLoading ? <Stack.Screen name="Loading" component={LoadingPage} options={{headerShown:false}}/> : null}
+					{!logueado ? 
 						// <Stack.Screen name="SignLogin" component={LoadingPage} options={{headerShown:false}}/> 
 						<Stack.Screen name="SignLogin" component={NavLogSign} options={{headerShown:false}}/> 
-
 						: 
 						<Stack.Screen name="MainNavigator" component={MainNav} options={{headerShown:false}}/>
 					}
@@ -37,6 +64,7 @@ export default function App() {
 					<Stack.Screen name="VideoPlayer" component={VideoPlayer} options={{headerShown:false}}/>
 					<Stack.Screen name="MoviePlayer" component={MoviePlayer} options={{headerShown:false}}/> */}
 			</Stack.Navigator>
+			</UserContext.Provider>
 		</NavigationContainer>
 	);
 }
